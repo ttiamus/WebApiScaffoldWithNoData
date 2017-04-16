1baf8e9f6ac5cc3ec529ef32e7b5e5c7c6603c38
Ttiamus <Ttiamus@aim.com>
2017-04-15 19:09:10 -0500

    rename script working!

---
+++ scaffold.ps1
@@ -0,0 +1,82 @@
+<#
+	The ReplaceInFiles function will find files based on a $filter, look for all occurances of a $pattern
+	and replace the matching text with $replace
+#>
+
+Param (
+	[Parameter(Mandatory=$True)]
+	[string]$solution
+)
+
+function CopyFiles($destination)
+{
+    
+    Get-ChildItem -filter * -exclude scaffold.ps1 -recurse | 
+        Where {
+            $_.FullName -notlike "*.git\*" `
+            -and (!($_.PSIsContainer)) ` 
+        } | foreach{
+    
+            $dir = $_.DirectoryName.Replace($PSScriptRoot,$destination)
+            $target = $_.FullName.Replace($PSScriptRoot,$destination)
+ 
+            if (!(test-path($dir))) { mkdir $dir }
+ 
+            if (!(test-path($target)))
+            {
+                copy-item -path $_.FullName -destination $target -recurse -force
+            }
+        }
+}
+
+function ReplaceInFiles($source, $pattern, $replace)
+{
+    echo "renaming files"
+
+	$files = Get-ChildItem -Path $source -filter * -exclude scaffold.ps1 -recurse | 
+		Where { ($_.FullName -inotmatch ("^$root\packages" -replace "\\","\\") `
+            -and $_.FullName -notlike "*.git\*" `
+			-and !$_.PSIsContainer `
+			-and ($_.extension -ne ".dll") `
+			-and ($_.extension -ne ".pdb") `
+			-and ($_.extension -ne ".exe")) ` 
+		}
+			
+	foreach($file in $files) 
+	{
+		(Get-Content $file.FullName) -replace $pattern, $replace | Out-File $file.FullName -Encoding ascii
+        $file | Rename-Item -NewName { $_.Name -replace $pattern, $replace }
+	}
+
+    echo "renaming folders"
+
+    Get-ChildItem -Path $source -filter * -Recurse |
+        Where {
+            $_.PSIsContainer `
+            -and $_.Name.Contains($pattern) `
+        } | Rename-Item -NewName { $_.Name -replace $pattern, $replace }
+
+    #Dir |
+    #    Where-Object { $_.Name.Contains($pattern) } |
+    #    Rename-Item -NewName { $_.Name -replace $pattern, $replace }
+}
+
+
+#ReplaceInFiles * "Rename.Me" $solution
+
+$solutionFolder = "..\$solution"
+
+echo "Checking for solution folder"
+if(!(Test-Path $solutionFolder))
+{
+    echo "creating solution folder"
+    New-Item -ItemType DIRECTORY -Path $solutionFolder
+    CopyFiles $solutionFolder
+
+    echo "Renaming"
+    ReplaceInFiles $solutionFolder "Rename.Me" $solution
+}
+else
+{
+    echo "Solution folder already exists"
+}
